0009186e8419380858aa23013edd2a761b346cb5
Terkel Gjervig <terkel@terkel.com>
2015-06-25 21:36:09 +0200

    Started on Photo section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+
+/*
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'awesome',
+    clientId: '4cc9716d969b44d3abeb1f4751a4586c',
+    template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>'
+});
+feed.run();
+*/
+
+/* DMJXintro15 */
