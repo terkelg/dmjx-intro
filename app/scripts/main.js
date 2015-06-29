eab471f6835ff3cdf90db36a3d58ff2a9fc3e106
Terkel Gjervig <terkel@terkel.com>
2015-06-29 16:16:52 +0200

    Added Tutor section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,45 @@
-console.log('\'Allo \'Allo!');
+'use strict';
 
-/*
-var feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'awesome',
-    clientId: '4cc9716d969b44d3abeb1f4751a4586c',
-    template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>'
+$('document').ready(function(){
+
+    var $loadButton, $loader, feed;
+
+
+    // grab out load more button and loader
+    $loadButton = $('.js-load-more');
+    $loader = $('.js-loader');
+
+    feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        resolution: 'low_resolution',
+        limit: 8,
+        clientId: '4cc9716d969b44d3abeb1f4751a4586c',
+        template: '<li class="photo"><a class="border" href="{{link}}" title="{{caption}}"><img src="{{image}}" alt="Instagram billede"></a></li>',
+        // every time we load more, run this function
+        after: function() {
+            $loader.addClass('hide');
+            $loadButton.removeClass('hide');
+            // disable button if no more results to load
+            if (!this.hasNext()) {
+                $loadButton.addClass('hide');
+            }
+        }
+    });
+
+    // bind the load more button
+    $loadButton.on('click', function() {
+        console.log('You Clicked ...');
+        $loadButton.addClass('hide');
+        $loader.removeClass('hide');
+        feed.next();
+    });
+
+    // run our feed!
+    feed.run();
+
 });
-feed.run();
-*/
 
-/* DMJXintro15 */
+/* DMJXintro15
+ * Add likes/description on hover
+ */
